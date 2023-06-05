6cdc237c9e202458d17ab7420be58bdd2ea926d3
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-06-05 19:14:23 +0400

    Реализовал отправку данных после оформления заказа в sessionStorage, так как сервис базы данных платный

---
+++ src/components/Info.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import stylesDrawer from "./Drawer/Drawer.module.scss";
+import AppContext from "../context";
+function Info({ image, title, description }) {
+  const { setCartOpened, cartOpened } = useContext(AppContext);
+  return (
+    <div>
+      <div className={stylesDrawer.cartEmpty}>
+        <div className={stylesDrawer.cartEmptyContainer}>
+          <img src={image} alt="Empty" />
+          <h3>
+            <b>{title}</b>
+          </h3>
+          <h5>{description}</h5>
+          <button onClick={() => setCartOpened(!cartOpened)}>
+            Вернуться назад!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
